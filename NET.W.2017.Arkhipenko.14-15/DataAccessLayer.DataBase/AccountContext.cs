f15aae046d9143114a3e707a2005e2a76f53bed3
alexandra26arkhipenko <32879488+alexandra26arkhipenko@users.noreply.github.com>
2017-12-03 22:34:40 +0300

    Add db

---
+++ NET.W.2017.Arkhipenko.14-15/DataAccessLayer.DataBase/AccountContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace DataAccessLayer.DataBase
+{
+    public class AccountContext : DbContext
+    {
+        public AccountContext() : base ("Accounts")
+        { }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AccountOwner> AccountOwners { get; set; }
+        public DbSet<AccountType> AccountTypes { get; set; }
+         
+    }
+}
